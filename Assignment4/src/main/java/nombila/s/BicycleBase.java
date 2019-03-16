c5c37edf03a1b46f6886ed9cf22f9c8bd0b0578e
Sokuphumlela Nomila <nombila.s@gmail.com>
2019-03-17 00:57:31 +0200

    Create BicycleBase.java

---
+++ Assignment4/src/main/java/nombila/s/BicycleBase.java
@@ -0,0 +1,32 @@
+package nombila.s;
+
+import bicycles.Bicycle;
+import bicycles.BikeRide;
+
+public abstract class BicycleBase implements Bicycle {
+
+    protected int speed;
+
+    protected void changeSpeed( int addToSpeed){
+
+         speed += addToSpeed;
+    }
+
+    public int currentSpeed() {
+
+        if (speed < 0) {
+            return 0;
+        } else {
+
+            return speed;
+        }
+    }
+
+    public void stop(){
+
+
+        speed = 0;
+//            return speed;
+    }
+
+}
