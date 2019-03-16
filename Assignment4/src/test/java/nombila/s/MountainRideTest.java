eff49c33a4e8c5733849f1a85f85aaf8a84bc97b
Sokuphumlela Nomila <nombila.s@gmail.com>
2019-03-17 01:08:04 +0200

    Update and rename AppTest.java to MountainRideTest.java

---
+++ Assignment4/src/test/java/nombila/s/MountainRideTest.java
@@ -0,0 +1,23 @@
+package nombila.s;
+
+import assignment4.MountainBike;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class BikeRideTest {
+    @Test
+    public void shouldRunRoadOne() {
+        RoadBike bicycle = new RoadBike();
+        BikeRideOne bike = new BikeRideOne(bicycle);
+        bike.ride();
+        System.out.println(bike.currentSpeed());
+        assertEquals(bike.currentSpeed(), 36);
+    }
+    @Test
+    public void shouldRunMountainOne() {
+        MountainBike bicycle = new MountainBike();
+        BikeRideOne bike = new BikeRideOne(bicycle);
+        bike.ride();
+        System.out.println(bike.currentSpeed());
+        assertEquals(bike.currentSpeed(), 14);
+    }
+}
